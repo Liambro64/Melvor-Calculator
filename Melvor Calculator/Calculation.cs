7dac1ecf5cba0eaece6dd3d011a47fc0153cd1a1
Liambro64 <86460132+Liambro64@users.noreply.github.com>
2022-11-17 08:28:04 +1030

    Initial file commit

---
+++ Melvor Calculator/Calculation.cs
@@ -0,0 +1,182 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+namespace Melvor_Calculator
+{
+    public class Calculation
+    {
+        static void print(object s)
+        {
+            Console.WriteLine(s);
+        }
+        static float LevelCalculator(float TargetLevel, float CurrentEXP, out float T)
+        {
+            float L = TargetLevel;
+
+            float TotalEXP = (float)(MathF.Pow(L, 2f) - L + 600f * ((MathF.Pow(2f, L / 7f) - MathF.Pow(2f, 1f / 7f)) / (MathF.Pow(2f, 1f / 7f) - 1f))) / 8;
+            T = TotalEXP;
+            print("Current EXP is: " + CurrentEXP);
+            TotalEXP -= CurrentEXP;
+            print("Target level: " + TargetLevel + "\nEXP needed to reach target level: " + MathF.Round(TotalEXP));
+            return TotalEXP;
+        }
+        static float ExpPerSecondCalculator(float ExpPerCompletion, float SecondsPerCompletion, float CompletionRate, out float TEPS, out float NCR)
+        {
+            float ExpPerSecond;
+            if (SecondsPerCompletion == 1)
+            {
+                 ExpPerSecond = ExpPerCompletion;
+            }
+            else
+            {
+                ExpPerSecond = ExpPerCompletion / SecondsPerCompletion;
+            }
+            print(ExpPerSecond);
+            float NewCompletionRate = 1 / (CompletionRate / 100);
+            NCR = NewCompletionRate;
+            //1 over ___
+            print("1\n-\n"+NewCompletionRate);
+            float TrueExpPerSecond = ExpPerSecond / NewCompletionRate;
+            TEPS = TrueExpPerSecond;
+            //print("Your 'true' exp per second " + TrueExpPerSecond + "/s");
+            return TrueExpPerSecond;
+        }
+        static void TimeToGetLevelsCalculator(float TrueExpPerSecond, float TotalEXP, float SecondsPerCompletion, out float Time, out float TotalCompletions)
+        {
+
+            Time = TotalEXP / TrueExpPerSecond;
+            TotalCompletions = Time / SecondsPerCompletion;
+            //print("It will take " + MathF.Ceiling(TotalCompletions) + " Completions");
+        }
+        void GetInput(out float a, out float b, out float c, string? p = "")
+        {
+            try
+            {
+                Console.WriteLine("Enter your current level: ");
+                a = (float)Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter your current EXP level (optional): ");
+                ConsoleKey k = Console.ReadKey().Key;
+                if (k != ConsoleKey.Enter)
+                {
+                    p = k.ToString()[1] + Console.ReadLine();
+
+                    b = (float)Convert.ToDouble(p);
+                }
+                else
+                    b = -1;
+                Console.WriteLine("Enter your target level: ");
+                c = (float)Convert.ToDouble(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error: user too dumb and put in something other than a number");
+                GetInput(out a, out b, out c);
+            }
+        }
+        void GetInput2(out float a, out float b, out float c)
+        {
+            try
+            {
+                Console.WriteLine("Enter the exp gain per completion: ");
+                a = (float)Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter your time per completion (seconds): ");
+                b = (float)Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter your completion percent (optional & full number not decimal): ");
+                ConsoleKey k = Console.ReadKey().Key;
+                if (k != ConsoleKey.Enter)
+                    c = (float)Convert.ToDouble((k.ToString() + Console.ReadLine()));
+                else
+                    c = 100;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error: user too dumb and put in something other than a number");
+                GetInput2(out a, out b, out c);
+            }
+        }
+        public static string FullCalculator(float EL, float TL, float EGPC, float TPC, float CR, out float TEPS, out float TotalCompletions,out float NCR, out float TotalEXP)
+        {
+            float Time;
+            if (CR >= 0)
+            {
+                CR = 100;
+            }
+            //GetInput(out CL, out EL, out TL);
+            //GetInput2(out EGPC, out TPC, out CR);
+            TimeToGetLevelsCalculator(
+                ExpPerSecondCalculator(EGPC, TPC, CR, out TEPS, out NCR),
+                LevelCalculator(TL, EL, out TotalEXP),
+                TPC, out Time, out TotalCompletions);
+
+            if (Time / 60 >= 1)
+            {
+                if (Time / 60 / 60 >= 1)
+                {
+                    if (Time / 60 / 60 / 24 >= 1)
+                    {
+                        if (Time / 60 / 60 / 24 / 7 >= 1)
+                        {
+                            if (Time / 60 / 60 / 24 / 7 / (52 / 12f) >= 1)
+                            {
+                                if (Time / 60 / 60 / 25 / 7 / 52 >= 1)
+                                {
+                                    int years = (int)Time / 4492800;
+                                    int months = ((int)Time % 4492800) / 345600;
+                                    int weeks = ((int)Time % 345600) / 604800;
+                                    int days = ((int)Time % 604800) / 86400;
+                                    int a = ((int)Time % 86400) / 3600;
+                                    int b = ((int)Time % 3600) / 60;
+                                    float c = Time % 60;
+                                    return ("It will take you " + years + " years, " + months + " months, " + weeks + " weeks, " + days + " days, " + a + " hours, " + b + " minutes, " + MathF.Round(c) + " seconds to complete");
+                                }
+                                else
+                                {
+                                    int months = ((int)Time) / 345600;
+                                    int weeks = ((int)Time % 345600) / 604800;
+                                    int days = ((int)Time % 604800) / 86400;
+                                    int a = ((int)Time % 86400) / 3600;
+                                    int b = ((int)Time % 3600) / 60;
+                                    float c = Time % 60;
+                                    return ("It will take you " + months + " months, " + weeks + " weeks, " + days + " days, " + a + " hours, " + b + " minutes, " + MathF.Round(c) + " seconds to complete");
+                                }
+                            }
+                            else
+                            {
+                                int weeks = ((int)Time / 604800);
+                                int days = ((int)Time % 604800) / 86400;
+                                int a = ((int)Time % 86400) / 3600;
+                                int b = ((int)Time % 3600) / 60;
+                                float c = Time % 60;
+                                return ("It will take you " + weeks + " weeks, " + days + " days, " + a + " hours, " + b + " minutes, " + MathF.Round(c) + " seconds to complete");
+                            }
+                        }
+                        else
+                        {
+                            int days = (int)Time / 86400;
+                            int a = ((int)Time % 86400) / 3600;
+                            int b = ((int)Time % 3600) / 60;
+                            float c = Time % 60;
+                            return ("It will take you " + days + " days, " + a + " hours, " + b + " minutes, " + MathF.Round(c) + " seconds to complete");
+                        }
+                    }
+                    else
+                    {
+                        int a = (int)Time / 3600;
+                        int b = ((int)Time % 3600) / 60;
+                        float c = Time % 60;
+                        return ("It will take you " + a + " hours, " + b + " minutes, " + MathF.Round(c) + " seconds to complete");
+                    }
+                }
+                else
+                {
+                    int a = (int)Time / 60;
+                    float b = Time % 60;
+                    return ("It will take you " + a + " minutes, " + MathF.Round(b) + " seconds to complete");
+                }
+            }
+            else
+            {
+                return ("It will take you " + MathF.Round(Time) + " seconds to complete");
+            }
+        }
+    }
+}
---
+++ Melvor Calculator/Form1.cs
@@ -0,0 +1,182 @@
+namespace Melvor_Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1() : base()
+        {
+            InitializeComponent();
+            this.Load += new EventHandler(Form1_Load);
+        }
+        float CompletionChance, timePerCompetion, expPerCompletion, targlev, exp;
+        float TrueExpPerSecond, TotalCompletions;
+        /// <summary>
+        /// //////////////////
+        /// </summary>
+        /// <param name="t"></param>
+        void add(Control t)
+        {
+            this.Controls.Add(t);
+
+        }
+        int i = 0;
+        //used for exp
+        TextBox t = new TextBox();
+        TextBox TL= new TextBox();
+        TextBox EPC= new TextBox();
+        TextBox TPC= new TextBox();
+
+        Label l1 = new Label();
+        Label l2 = new Label();
+        Label l3 = new Label();
+        Label l4 = new Label();
+        Label l5 = new Label();
+        Label l6 = new Label();
+        Label l7 = new Label();
+        private void Form1_Load(object sender, EventArgs e) {
+            //1
+            t.Location = new Point(100,50);
+            t.BackColor = Color.LightGray;
+            t.ForeColor = Color.DarkOliveGreen;
+            t.AutoSize = true;
+            t.Name = "enter EXP";
+            t.Text = "";
+            t.KeyPress += new KeyPressEventHandler(t_KeyPress);
+            add(t);
+            //2
+            TL.Location = new Point(100, 100);
+            TL.BackColor = Color.LightGray;
+            TL.ForeColor = Color.DarkOliveGreen;
+            TL.AutoSize = true;
+            TL.Name = "enter Target level";
+            TL.Text = "";
+            TL.KeyPress += new KeyPressEventHandler(TL_KeyPress);
+            add(TL);
+            //3
+            EPC.Location = new Point(100, 150);
+            EPC.BackColor = Color.LightGray;
+            EPC.ForeColor = Color.DarkOliveGreen;
+            EPC.AutoSize = true;
+            EPC.Name = "enter Target level";
+            EPC.Text = "";
+            EPC.KeyPress += new KeyPressEventHandler(EPC_KeyPress);
+            add(EPC);
+            //4
+            TPC.Location = new Point(100, 200);
+            TPC.BackColor = Color.LightGray;
+            TPC.ForeColor = Color.DarkOliveGreen;
+            TPC.AutoSize = true;
+            TPC.Name = "enter Target level";
+            TPC.Text = "";
+            TPC.KeyPress += new KeyPressEventHandler(TPC_KeyPress);
+            add(TPC);
+            l1.Location = new Point(50, 50);
+            l1.Text = "EXP";
+            add(l1);
+            l2.Location = new Point(50, 100);
+            l2.Text = "Target";
+            add(l2);
+            l3.Location = new Point(50, 150);
+            l3.Text = "Exp Per";
+            add(l3);
+            l4.Location = new Point(50, 200);
+            l4.Text = "Time Per";
+            add(l4);
+            l5.Location = new Point(200, 200);
+            l5.Text = "Nothing yet";
+            l5.AutoSize = true;
+            add(l5);
+            l6.Location = new Point(200, 250);
+            l6.Text = "Nothing yet";
+            l6.AutoSize = true;
+            add(l6);
+            l7.Location = new Point(200, 300);
+            l7.Text = "Nothing yet";
+            l7.AutoSize = true;
+            add(l7);
+        }
+
+        private void TPC_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+                (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+            try
+            {
+                timePerCompetion = (float)Convert.ToDouble(EPC.Text + e.KeyChar.ToString());
+                CheckCalculation();
+            }
+            catch
+            {
+
+            }
+        }
+        private void EPC_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+                (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+            try
+            {
+                expPerCompletion = (float)Convert.ToDouble(EPC.Text + e.KeyChar.ToString());
+                CheckCalculation();
+            }
+            catch
+            {
+
+            }
+        }
+        private void TL_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+                (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+            try
+            {
+                targlev = (float)Convert.ToDouble(TL.Text+e.KeyChar.ToString());
+                //l5.Text = TL.Text+e.KeyChar.ToString();
+                CheckCalculation();
+            }
+            catch
+            {
+
+            }
+        }
+        int j = 0;
+        private void t_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+                (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+            try
+            {
+                exp = (float)Convert.ToDouble(t.Text + e.KeyChar.ToString());
+                CheckCalculation();
+            }
+            catch
+            {
+
+            }
+        }
+        void CheckCalculation()
+        {
+            CompletionChance = 100;
+            try
+            {
+                l5.Text = Calculation.FullCalculator(exp, targlev, expPerCompletion,  timePerCompetion, CompletionChance, out TrueExpPerSecond, out TotalCompletions,out float n, out float penis);
+                l5.Text = penis.ToString();
+                l6.Text = n.ToString();
+                l7.Text = TrueExpPerSecond.ToString();
+            }
+            catch { }
+        }
+
+    }
+}
